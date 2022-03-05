b0d8af66d6c90abbf8b6e28d32d86913af0bb2b6
Caio Gomes <caicesardev@gmail.com>
2022-03-05 04:27:31 +0100

    Moved to main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import sys
 import os
 import subprocess
+import youtube_dl
+
 from ui.ui_MainWindow import Ui_MainWindow
 from about import About
 
 from pytube import YouTube
+from hurry.filesize import size, si
 
-from PySide6.QtGui import QIcon
 from PySide6.QtCore import (
     QLibraryInfo,
     QLocale,
@@ -29,25 +31,31 @@ __version__ = "1.0.0"
 class Worker(QObject):
     finished = Signal()
     progress = Signal(int)
+    speed = Signal(float)
 
     def __init__(self, url):
         super(Worker, self).__init__()
 
-        print("Init")
         self.url = url
-        self.yt = YouTube(self.url, on_progress_callback=self.report_progress)
-        self.yv = self.yt.streams.get_highest_resolution()
-        print("Finish init")
 
     def run(self):
-        print("Downloading")
-        self.yv.download("C:/users/Edu/Downloads")
-        print("Finished download")
+        windows_dir = "C:/users/%username%/Downloads/%(title)s.%(ext)s"
+        linux_dir = "/home/downloads/%(title)%s.%(ext)s"
+        ydl_opts = {
+            "progress_hooks": [self.callable_hook],
+            'outtmpl': windows_dir if os.name == "nt" else linux_dir
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.url])
         self.finished.emit()
 
-    def report_progress(self, stream, chunk, bytes_remaining):
-        self.progress.emit(round((1-bytes_remaining/self.yv.filesize)*100, 3))
-        print(round((1-bytes_remaining/self.yv.filesize)*100, 3), '% done...')
+    def callable_hook(self, response):
+        if response["status"] == "downloading":
+            speed = response["speed"]
+            downloaded_percent = (
+                response["downloaded_bytes"]*100)/response["total_bytes"]
+            self.progress.emit(downloaded_percent)
+            self.speed.emit(speed)
 
 
 # MainWindow.
@@ -60,8 +68,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.input.editingFinished.connect(
             self.handle_editing_finished)
 
-        self.input.returnPressed.connect(
-            lambda: self.update_menu_stream(self.input.text()))
+        # self.input.returnPressed.connect(
+        #    lambda: self.update_menu_stream(self.input.text()))
 
         # Download button.
         self.download_button.clicked.connect(self.download)
@@ -78,40 +86,39 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def download(self):
         try:
-            self.download_button.setEnabled(False)
-            self.select_strm_button.setEnabled(False)
-            self.download_button.setText("Actualmente descargando...")
+            self.yt = YouTube(self.input.text())
 
-            # YouTube downloader.
-            self.url = self.input.text()
-            self.yt = YouTube(self.url)
-            self.status_bar.showMessage(f"Descargando {self.yt.title} || 0%")
+            # https://www.youtube.com/watch?v=dP15zlyra3c <- For testings
+
+            self.download_button.setEnabled(False)
+            self.download_button.setText("Actualmente descargando...")
 
             # Create a QThread object.
             self.thread = QThread()
             # Create a worker object.
-            self.worker = Worker(self.url)
+            self.worker = Worker(self.input.text())
             # Move worker to the thread.
             self.worker.moveToThread(self.thread)
             # Connect signals and slots
             self.thread.started.connect(self.worker.run)
             self.worker.finished.connect(self.thread.quit)
-            self.worker.finished.connect(
-                lambda: self.on_download_finished(self.yt.title))
+            self.worker.finished.connect(self.on_download_finished)
             self.worker.finished.connect(self.worker.deleteLater)
             self.thread.finished.connect(self.thread.deleteLater)
 
             self.worker.progress.connect(self.update_progress_bar)
             self.worker.progress.connect(self.update_status_bar)
+            self.worker.speed.connect(self.update_speed_lbl)
 
             self.thread.start()
 
-        except Exception as e:
-            print(e)
+        except Exception:
+            self.download_button.setEnabled(True)
+            self.download_button.setText("Descargar")
             QMessageBox.information(
                 self,
                 "Enlace no válido",
-                "El enlace introducido no es válido o no se puede recopilar.")
+                "El enlace introducido no es válido o no se pudo recopilar.")
 
     def update_progress_bar(self, value):
         self.progress_bar.setValue(value)
@@ -119,23 +126,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def update_status_bar(self, value):
         self.status_bar.showMessage(f"Descargando {self.yt.title} || {value}%")
 
-    def on_download_finished(self, f_name):
+    def update_speed_lbl(self, value):
+        self.speed_label.setText(f"Velocidad: {size(value, system=si)}/s")
+
+    def on_download_finished(self):
         self.status_bar.showMessage("Descarga finalizada", 3000)
         self.progress_bar.setValue(0)
+        self.speed_label.clear()
         QMessageBox.information(
             self,
             "Descarga completada",
-            f"Se ha completado la descarga de {f_name}.mp4")
+            f"Se ha completado la descarga de {self.yt.title}.mp4")
         self.download_button.setText("Descargar")
         self.download_button.setEnabled(True)
-        self.select_strm_button.setEnabled(True)
 
     def handle_editing_finished(self):
         if self.input.isModified():
             self.url = self.input.text()
-            self.update_menu_stream(self.url)
+            # self.update_menu_stream(self.url)
         self.input.setModified(False)
 
+    """ 
     def update_menu_stream(self, url):
         # Streams menu.
         menu = QMenu(self)
@@ -146,6 +157,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 f'{stream}',
                 self.download)
         self.select_strm_button.setMenu(menu)
+    """
 
     def on_restart(self):
         self.close()
